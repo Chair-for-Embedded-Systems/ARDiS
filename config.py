86e2d163d97d69124b9aa2a57d8b88934e27e8fd
Mohammed Bakr Sikal <mohammed.bakr.sikal@gmail.com>
2024-07-11 16:25:31 +0200

    adding missing spec apps

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ RESULTS_FOLDER="./results/"
 UTILS_DIR="./utils"
 
 # List of available applications on the system
-available_apps = [#'spec-gcc', 'spec-milc', 'spec-bzip2', 'spec-sphinx3', 'spec-astar', 'spec-lbm',
-                  #'spec-bwaves', 'spec-mcf', 'spec-zeusmp',  'spec-namd', 'spec-h264ref', 'spec-gobmk',
-                  #'spec-povray', 'spec-gromacs', 'spec-cactusADM', 'spec-omnetpp', 'spec-hmmer', 'spec-leslie3d',
+available_apps = ['spec-gemsFDTD','spec-gcc', 'spec-milc', 'spec-bzip2', 'spec-sphinx3', 'spec-astar', 'spec-lbm',
+                  'spec-bwaves', 'spec-mcf', 'spec-zeusmp',  'spec-namd', 'spec-h264ref', 'spec-gobmk',
+                  'spec-povray', 'spec-gromacs', 'spec-cactusADM', 'spec-omnetpp', 'spec-hmmer', 'spec-leslie3d',
                   'parsec-blackscholes', 'parsec-bodytrack', 'parsec-canneal', 'parsec-dedup', 'parsec-facesim',
                   'parsec-ferret', 'parsec-fluidanimate', 'parsec-freqmine', 'parsec-streamcluster',
                   'parsec-swaptions', 
