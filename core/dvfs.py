e3b2e83f1b13fcc1749aba413dd6d9e5f10f4dd3
Mohammed Bakr Sikal <mohammed.bakr.sikal@gmail.com>
2024-07-15 17:19:39 +0200

    fixing dvfs policy naming and set frequency

--- core/dvfs.py
+++ core/dvfs.py
@@ -1,13 +1,13 @@
-from utils.intel_dvfs import *
+from utils.inteldvfs import *
 
 class DVFSPolicy():
     def __init__(self, debug=False):
         self.__debug = debug
-        self.__CPUFrequencyManager(self.__debug)
+        self.manager = CPUFrequencyManager(self.__debug)
 
     def setInitialFrequency(self, cores, frequency):
         for core in cores:
-            self.__CPUFrequencyManager.set_frequency(core, frequency)
+            self.manager.setFrequency(core, frequency)
             if self.__debug:
                 print(f"Core {core} set to {frequency} MHz")
     def executeDVFSPolicy(self, cores, frequency):
