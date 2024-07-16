afe48fa829f1a9758f0bbbc76f2fd6f132e5c747
Mohammed Bakr Sikal <mohammed.bakr.sikal@gmail.com>
2024-07-16 16:22:30 +0200

    structuring run.py

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from core.engine import *
 from core.scheduler import *
 from core.policies.consecutive_schedule import *
 from core.dvfs import *
+from core.policies.intel_motivational_mapping import *
 import time
 from timeit import default_timer as timer
 from random import randrange
@@ -14,7 +15,7 @@ class Experiment:
     def __init__(self, name="", applications=[]):
         self.__name = name
         self.__applications = applications
-        self.__engine = Engine(self.__name)
+        self.__engine = Engine(self.__name, IntelMotivationalExample(False))
         #self.__scheduler = Scheduler()
         # Create a schedule with a delay in the arrival time of 2.5 seconds between each application
         # use 0 for all applications to arrive at the same time
@@ -41,17 +42,37 @@ class Experiment:
         self.__engine.setStaticFrequency(frequency)
 
 
-
-if __name__ == "__main__":
-    # Create an experiment object
-    exp = Experiment("Simple Experiment")
-    # Generate a random list of applications
-    # exp.generateRandomApps(system_cores)
+def runExample():
+   # Create an experiment object
+    exp = Experiment("Simple Experiment with Specific Applications")
     # Manually set the applications to execute
     exp.setApplications(['parsec-ferret', 'parsec-canneal', 'parsec-streamcluster', 'parsec-freqmine'])
-    exp.setInitialFrequency(1500)
+    # Set the initial frequency of the cores
+    exp.setInitialFrequency(2900)
     # Run the experiment
     exp.executeExperiment()
+
+
+def runRandomExample():
+    # Create an experiment object
+    exp = Experiment("Simple Experiment with Random Applications")
+    # Generate a random list of applications
+    exp.generateRandomApps(system_cores)
+    # Set the initial frequency of the cores
+    exp.setInitialFrequency(2900)
+    # Run the experiment
+    exp.executeExperiment()
+
+def runMotivationalExample():
+    exp = Experiment("Motivational Example")
+    exp.setApplications(['parsec-ferret', 'parsec-canneal', 'parsec-streamcluster', 'parsec-freqmine'])
+    exp.setInitialFrequency(2900)
+    exp.executeExperiment()
+
+if __name__ == "__main__":
+    #runExample()
+    #runRandomExample()
+    runMotivationalExample()
 
 
 
