3de2302f2ad988e2ad08a6d51eebd8339f24c134
Mohammed Bakr Sikal <mohammed.sikal@kit.edu>
2024-07-18 16:16:20 +0200

    feat: take the default explicit mapping from the config if not provided

--- core/policies/explicit_mapping.py
+++ core/policies/explicit_mapping.py
@@ -1,9 +1,10 @@
 from core.mapping import *
+from config import *
 
 
 #Let the mapping come from a list of cores
 class ExplicitMapping(MappingPolicy):
-    def __init__(self, cores):
+    def __init__(self, cores = explicit_mapping_cores):
         super().__init__()
         self.__cores = cores
 
