86e0fbb38bfe544ad231d3fb6e403bc6640a510e
Mohammed Bakr Sikal <mohammed.sikal@kit.edu>
2024-08-16 10:32:59 +0200

    feat: tracking the LLC accesses and misses

--- config.py
+++ config.py
@@ -1,20 +1,47 @@
 import os
 
 RESULTS_FOLDER="./results/characterization/"
+#RESULTS_FOLDER="./results/exps/"
 UTILS_DIR="./utils"
 ROOTPATH=os.getcwd()
 DEBUG = False
 #DEBUG = True
 # List of available applications on the system
-available_apps = ['spec-GemsFDTD','spec-gcc', 'spec-milc', 'spec-bzip2', 'spec-sphinx3', 'spec-astar', 'spec-lbm',
-                  'spec-bwaves', 'spec-mcf', 'spec-zeusmp', 'spec-namd', 'spec-h264ref', 'spec-gobmk', 'spec-libquantum',
-                  'spec-povray', 'spec-gromacs', 'spec-cactusADM', 'spec-omnetpp', 'spec-hmmer', 'spec-leslie3d',
-                  #'parsec-blackscholes', 'parsec-bodytrack', 'parsec-canneal', 'parsec-dedup', 'parsec-facesim',
-                  #'parsec-ferret', 'parsec-fluidanimate', 'parsec-freqmine', 'parsec-streamcluster',
-                  #'parsec-swaptions', 
-                  #'parsec-vips', 
-                  #'parsec-x264'
-                  ]
+
+available_apps = [
+    'spec-GemsFDTD',
+    #'spec-gcc',
+    'spec-milc',
+    'spec-bzip2',
+    'spec-sphinx3',
+    'spec-astar',
+    'spec-lbm',
+    'spec-bwaves',
+    'spec-mcf',
+    'spec-zeusmp',
+    'spec-namd',
+    'spec-h264ref',
+    'spec-gobmk',
+    #'spec-libquantum',
+    'spec-povray',
+    'spec-gromacs',
+    'spec-cactusADM',
+    'spec-omnetpp',
+    'spec-hmmer',
+    'spec-leslie3d',
+    #'parsec-blackscholes',
+    #'parsec-bodytrack',
+    #'parsec-canneal',
+    #'parsec-dedup',
+    #'parsec-facesim',
+    #'parsec-ferret',
+    #'parsec-fluidanimate',
+    #'parsec-freqmine',
+    #'parsec-streamcluster',
+    #'parsec-swaptions',
+    #'parsec-vips',
+    #'parsec-x264'
+]
 
 # Number of cores in the system
 system_cores = 24
@@ -30,6 +57,5 @@ action_interval = 0.1
 explicit_mapping_cores = [2, 4, 17, 23]
 
 # Monitoring parameters
-sampling_rate = 100 # in ms
-events_to_track = ["instructions", "cache-misses", "cache-references","power/energy-pkg/", "power/energy-cores/", "power/energy-psys/"]
+events_to_track = ["instructions", "cache-misses", "cache-references", "LLC-loads", "LLC-load-misses", "LLC-stores", "LLC-store-misses", "power/energy-pkg/", "power/energy-cores/", "power/energy-psys/"]
 enable_monitoring = True 
