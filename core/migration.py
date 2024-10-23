1dbd186e8800503b7907637f4ffcc94773c5f712
Mohammed Bakr Sikal <mohammed.sikal@kit.edu>
2024-10-23 17:54:35 +0200

    feat: organizing migration policies with inheritance

--- core/migration.py
+++ core/migration.py
@@ -1,14 +1,10 @@
-import threading
-import subprocess
 import random
+import subprocess
 import config
-import json
-
-
 
 class MigrationPolicy:
-    def __init__(self, static_schedule):
-        self.static_schedule = static_schedule
+    def __init__(self):
+        pass
     
 
     def __setAffinity(self, pid, core):
@@ -37,58 +33,31 @@ class MigrationPolicy:
                 except:
                     tmp_pids[app] = -1                 
         return tmp_pids
+
+    # this is a no-op for the base class
+    def getNewMapping(self, instructions, mapping):
+        return mapping
     
-    def getShuffledMapping(self, mapping):
-        tmp_mapping = mapping.copy()
-        available_cores = list(mapping.values())
-        random.shuffle(available_cores)
-        for app in mapping:
-            tmp_mapping[app] = available_cores.pop()
-        return tmp_mapping
-    
-    def getFixedMapping(self, mapping):
-        tmp_mapping = mapping.copy()
-        ctr = 2
-        for app in mapping:
-            tmp_mapping[app] = ctr
-            ctr += 2
-        return tmp_mapping
-    
-    def getRandomMapping(self, mapping):
-        tmp_mapping = mapping.copy()
-        available_cores = list(range(2, config.system_cores))
-        random.shuffle(available_cores)
-        for app in mapping:
-            tmp_mapping[app] = available_cores.pop()
-        return tmp_mapping
-    
-
-    def getStaticScheduleMapping(self, instructions, mapping):
-        tmp_mapping = mapping.copy()
-        for app in tmp_mapping:
-            selected_core = tmp_mapping[list(tmp_mapping.keys())[0]]  # Default to current core
-            current_phase = None
-            for entry in self.static_schedule:
-                if instructions >= entry["trigger_instruction"]:
-                    selected_core = config.intel_e_core_ids[0] if "E-core" in entry["core"] else config.intel_p_core_ids[3]
-                    current_phase = entry["phase"]
-                else:
-                    break  # Stop once the correct phase is determined
-
-            if tmp_mapping[app] != selected_core:
-                tmp_mapping[app] = selected_core
-                if config.DEBUG:
-                    print("[Migration Policy] Now entering Phase: ", current_phase, "Core: ", selected_core)
-        
-        return tmp_mapping
-
-
-
-# # Let's test the migration policy
-# migration = MigrationPolicy()
-# mapping  = {"app1": 0, "app2": 2, "app3": 4, "app4": 6, "app5": 8, "app6": 10}
-# pids = {"app1": 100, "app2": 200, "app3": 300, "app4": 400, "app5": 500, "app6": 600}
-# newmapping = migration.getShuffledMapping(mapping)
-# print("original map is", mapping)
-# print("new map is", newmapping)
-# print(migration.executeMigration(mapping, newmapping, pids))
+    ##def getShuffledMapping(self, mapping):
+    ##    tmp_mapping = mapping.copy()
+    ##    available_cores = list(mapping.values())
+    ##    random.shuffle(available_cores)
+    ##    for app in mapping:
+    ##        tmp_mapping[app] = available_cores.pop()
+    ##    return tmp_mapping
+    ##
+    ##def getFixedMapping(self, mapping):
+    ##    tmp_mapping = mapping.copy()
+    ##    ctr = 2
+    ##    for app in mapping:
+    ##        tmp_mapping[app] = ctr
+    ##        ctr += 2
+    ##    return tmp_mapping
+    ##
+    ##def getRandomMapping(self, mapping):
+    ##    tmp_mapping = mapping.copy()
+    ##    available_cores = list(range(2, config.system_cores))
+    ##    random.shuffle(available_cores)
+    ##    for app in mapping:
+    ##        tmp_mapping[app] = available_cores.pop()
+    ##    return tmp_mapping
