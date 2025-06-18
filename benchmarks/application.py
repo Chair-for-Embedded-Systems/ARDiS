2747221d77f14ee8fdd7a39e816ef51f51f941d5
Andreas Noebel <uhqql@student.kit.edu>
2025-06-18 09:25:38 +0200

    feat: add abtract class for an application

---
+++ benchmarks/application.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class Application(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def execute(self, cores: set[int] | None) -> None:
+        """
+        Starts the execution of the application and blocks until it has finished.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_pid(self) -> int | None:
+        """
+        Returns the PID of this application,
+        may return None if the pid could not be determined.
+        """
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_display_name(self) -> str:
+        """
+        Returns the display name for the application.
+        This is the name that will be used in the log files
+        """
+        raise NotImplementedError
