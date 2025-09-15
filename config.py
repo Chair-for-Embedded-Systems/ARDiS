c4ab4ce4eec0f3813e63f38e32082d5730b7d8bf
Andreas Noebel <uhqql@student.kit.edu>
2025-09-15 10:20:33 +0200

    chore: add clock domains to config

--- config.py
+++ config.py
@@ -80,6 +80,8 @@ parsec_network_apps = [
 ]
 # Number of cores in the system
 system_cores = 24
+clock_domains = [{0, 1}, {2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}, {12, 13}, {14, 15},
+                 {16, 17, 18, 19}, {20, 21, 22, 23}]
 # platform specific stuff
 intel_p_core_ids = [0, 2, 4, 6, 8, 10, 12, 14]
 intel_e_core_ids = [16, 17, 18, 19, 20, 21, 22, 23]
