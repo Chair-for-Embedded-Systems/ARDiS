b9948a45abd0e117b20b5daec78d43803cb803e0
Andreas Noebel <uhqql@student.kit.edu>
2025-09-18 20:33:38 +0200

    chore: remove debug code

--- core/cpu/intel/frequency_manager.py
+++ core/cpu/intel/frequency_manager.py
@@ -40,7 +40,6 @@ class IntelFrequencyManager(CPUFrequencyManager):
                 self._set_pstate(core, p_state)
             return
             
-    
     def get_cpu_freq(self, core: int) -> float:
         return super().get_cpu_freq(core)
 
@@ -110,85 +109,3 @@ class IntelFrequencyManager(CPUFrequencyManager):
                 self._hwp_interface.set_hwp_request(core, hwp_request)
         
         super().restore_initial_state()
-
-
-    def _print_core_stats(self, core: int):
-        print(f"Core: {core}")
-        print(f"Scaling driver: {self.get_scaling_driver(core)}")
-        print(f"Current governor: {self.get_governor(core)}")
-        output = [
-            "==============================",
-            f" Core: {core}",
-            "------------------------------",
-            f" Scaling driver: {self.get_scaling_driver(core)}",
-            f" Current governor: {self.get_governor(core)}"
-        ]
-
-        try:
-            scaling_min_khz, scaling_max_khz = self.get_scaling_limits(core)
-            output.append(f" Scaling Min: {scaling_min_khz / 1000:.1f} MHz")
-            output.append(f" Scaling Max: {scaling_max_khz / 1000:.1f} MHz")
-        except Exception as e:
-            output.append(f" Failed to get scaling limits: {e}")
-
-        try:
-            cpu_min_khz, cpu_max_khz = self.get_processor_limits(core)
-            output.append(f" CPU Min: {cpu_min_khz / 1000:.1f} MHz")
-            output.append(f" CPU Max: {cpu_max_khz / 1000:.1f} MHz")
-        except Exception as e:
-            output.append(f" Failed to get processor limits: {e}")
-
-        if hasattr(self, "_hwp_interface"):
-            hwp_capabilities = self._hwp_interface.get_hwp_capabilities(core)
-            if hwp_capabilities:
-                output.append(f" HWP Capabilities: {hwp_capabilities}")
-            hwp_request = self._hwp_interface.get_hwp_request(core)
-            if hwp_request:
-                output.append(f" HWP Request: {hwp_request}")
-
-        try:
-            available_governors = self.get_available_governors(core)
-            output.append(f" Available governors: {available_governors}")
-        except Exception as e:
-            output.append(f" Failed to get available governors: {e}")
-
-        output.append("==============================")
-        print("\n".join(output))
-
-if __name__ == "__main__":
-    from config import clock_domains
-    freq_manager = IntelFrequencyManager(clock_domains=clock_domains)
-    
-    freq_manager._print_core_stats(0) # type: ignore
-    
-    #print(freq_manager.get_governor(2))
-    #print(freq_manager.get_available_governors(2))
-    #freq_manager.set_governor(16, "userspace")
-    #freq_manager.set_governor(17, "userspace")
-    #freq_manager.set_governor(18, "userspace")
-    #freq_manager.set_governor(19, "userspace")
-    #freq_manager.set_governor(3, "userspace")
-    
-    if False:
-        p_state = 12
-        freq_manager._set_pstate(3, p_state)
-        freq_manager._set_pstate(2, p_state)
-    #freq_manager.set_scaling_limits(2, 1000, 2000)
-    #freq_manager.set_scaling_limits(3, 1000, 2000)
-
-    #freq_manager._print_core_stats(2)
-    if False:
-        freq = 2000 
-        freq_manager.set_scaling_limits(16, freq, freq)
-        freq_manager.set_scaling_limits(17, freq, freq)
-        freq_manager.set_scaling_limits(18, freq, freq)
-        freq_manager.set_scaling_limits(19, freq, freq)
-        #freq_manager.set_scaling_limits(3, freq, freq)
-    
-    #freq_manager._print_core_stats(16)
-    #freq_manager._print_core_stats(17)
-    #freq_manager._print_core_stats(18)
-    #freq_manager._print_core_stats(19)
-    #print()
-    #freq_manager._print_core_stats(3)
-    freq_manager.restore_initial_state()
