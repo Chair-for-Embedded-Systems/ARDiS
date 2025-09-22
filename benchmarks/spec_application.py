4d8b3ad53106c2884455843122ebba8af367465f
Andreas Noebel <uhqql@student.kit.edu>
2025-09-22 22:52:36 +0200

    feat: update pid fetch mechanism for spec applications

--- benchmarks/spec_application.py
+++ benchmarks/spec_application.py
@@ -2,7 +2,6 @@ from enum import Enum
 import os
 import subprocess
 from benchmarks.application import Application
-from core.procworker import get_pid_of_app
 from config import SPECPBASE, SPEC_CONFIGFILE
 
 class SpecApplication(Application):
@@ -16,13 +15,15 @@ class SpecApplication(Application):
         super().__init__(labels)
         self._application_package = application_package
         self._input = input
+        self._process: subprocess.Popen[bytes] | None = None
+        self._shell_pid: int | None = None
 
     def _set_environment(self):
         if "SPECBIN" not in os.environ:
             # Source shrc and capture environment variables
             command = f"cd {SPECPBASE} && source ./shrc && env"
             proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, executable="/bin/bash")
-            output, error = proc.communicate()
+            output, _ = proc.communicate()
 
             # Parse output and update os.environ
             for line in output.decode().splitlines():
@@ -46,16 +47,48 @@ class SpecApplication(Application):
             command = f"runspec --iterations 1 --size {input_size} --action onlyrun --config {SPEC_CONFIGFILE} --noreportable {app} > {log_file}"
         
 
-        subprocess.run(command, shell=True, env=os.environ)
+        self._process = subprocess.Popen(command, shell=True, env=os.environ)
+        self._shell_pid = self._process.pid
+        self._process.wait()
 
     def get_pid(self) -> int | None:
-        # Return cached pid if available
-        if self._pid:
+        
+        # Return if a PID is already known and the process is still running
+        if self._pid and os.path.exists(f"/proc/{self._pid}"):
             return self._pid
         
-        if pid := get_pid_of_app(self._application_package, self._start_affinity):
-            self._pid = pid
-            return pid
+        # Try to find the PID in the process child tree of the shell process
+        if self._shell_pid is None:
+            return None
+        else:
+            self._pid = self._find_binary_in_tree_recursively(self._shell_pid, self._application_package)
+            return self._pid
+    
+    def _find_binary_in_tree_recursively(self, pid: int, binary_name: str) -> int | None:
+        """
+        Recursively search for a binary in the process tree starting from the given PID.
+        Returns the PID of the found binary or None if not found.
+        """
+        try:
+            with open(f"/proc/{pid}/task/{pid}/children", "r") as f:
+                children = f.read().strip().split()
+                for child_pid in children:
+                    child_pid = int(child_pid)
+                    executable_link = f"/proc/{child_pid}/exe"
+                    
+                    if os.path.islink(executable_link):
+                        target_path = os.readlink(executable_link)
+                        # Check if the target path contains the binary name
+                        if binary_name in os.path.basename(target_path):
+                            return child_pid
+
+                    result = self._find_binary_in_tree_recursively(child_pid, binary_name)
+                    if result is not None:
+                        return result
+        except Exception as _:
+            return None
+        
+        return None
     
     def get_display_name(self) -> str:
         return f"{self._application_package}_{self._input.value}"
