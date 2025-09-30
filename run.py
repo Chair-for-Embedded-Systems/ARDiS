95a3717437cb1b8f350171e8bdd68cbfca5b04c6
Andreas Noebel <uhqql@student.kit.edu>
2025-09-30 21:57:32 +0200

    chore: update example

--- run.py
+++ run.py
@@ -1,29 +1,43 @@
 from ardis.benchmarks import ParsecApplication
 from ardis.core import MonitoringMode
 from ardis.core.policies import ExplicitMapping, ConsecutiveScheduler, StaticDVFS
-from ardis import config
+from ardis.core.postprocessing.simple_clip_postprocessor import SimpleClipPostProcessor, Clips
 from ardis.experiments import Experiment
 
-
-def run_example_with_core_monitoring():
+# For additional examples, see the experiments directory in the repository root.
+def run_example():
    # Create an experiment object
     exp = Experiment(
         name="Simple Experiment with Specific Applications", 
-        mapping_policy=ExplicitMapping([{3}, {6}, {19}]),
+        mapping_policy=ExplicitMapping([{2}, {4}, {16}, {20}]),
         scheduler=ConsecutiveScheduler(0),
-        dvfs_policy=StaticDVFS({core: 3000 for core in range(config.system_cores)}),
-        monitoring_mode=MonitoringMode.PERIODIC_ON_CORE
+        dvfs_policy=StaticDVFS(
+            core_to_frequency_mhz={ 2: 4300, 4: 4000, 16: 3800, 20: 2800 },
+            base_frequency_mhz=3000
+        ),
+        monitoring_mode=MonitoringMode.PERIODIC_ON_PID
     )
-    
     # Manually set the applications to execute
     exp.setApplications([
         ParsecApplication("parsec.blackscholes"),
-        ParsecApplication("parsec.bodytrack"),
+        ParsecApplication("splash2x.fft"),
+        ParsecApplication("parsec.dedup"),
         ParsecApplication("splash2x.radix")
     ])
+    # Set the postprocessor to create specific clips
+    exp.setPostProcessor(SimpleClipPostProcessor(
+        clips=[
+            Clips.APP_EXECUTION_OVERVIEW, 
+            Clips.APP_METRICS_ALL,
+            Clips.SYSTEM_CORE_FREQUENCY,
+            Clips.SYSTEM_METRICS_ALL
+        ],
+        formats={"png"},
+        verbose=True
+    ))
     # Run the experiment
     exp.executeExperiment()
 
 if __name__ == "__main__":
-    run_example_with_core_monitoring()
+    run_example()
     
