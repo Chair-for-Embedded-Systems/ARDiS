02f3aba48d7300a8d8161669e9760b6140d8513a
Andreas Noebel <uhqql@student.kit.edu>
2026-01-11 12:17:08 +0100

    feat: add checks an exceptions to configuration parser

--- ardis/utils/config_parser.py
+++ ardis/utils/config_parser.py
@@ -1,24 +1,61 @@
 import ast
-import configparser
 import os
+
+from configparser import ConfigParser
+
+class ConfigLoadError(Exception):
+    """Something went wrong when loading the configuration file."""
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+class ConfigOptionError(Exception):
+    """An invalid option was specified in the configuration file."""
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
 
 class ARDISConfigParser:
 
-    def __init__(self, config_file: str) -> None:
+    _CONFIG_FOLDER = os.path.join(os.path.dirname(__file__), '../../configs/')
+    _DEFAULT_CONFIG_NAME = "ardis-config.ini"
+
+    @staticmethod
+    def _default_config_file_path() -> str:
+        
+        # Check if an environment variable is set for the config file
+        env_path = os.getenv("ARDIS_CONFIG_FILE")
+        if env_path:
+            return env_path
+        
+        # Get potential configurations from the default location
+        config_folder = ARDISConfigParser._CONFIG_FOLDER
+        config_name = ARDISConfigParser._DEFAULT_CONFIG_NAME
+        potential_configs = os.listdir(ARDISConfigParser._CONFIG_FOLDER)
+        if ARDISConfigParser._DEFAULT_CONFIG_NAME in potential_configs:
+            return os.path.join(config_folder, config_name)
+        
+        raise ConfigLoadError(
+            "No configuration file specified!\n"
+            f"Either set the ARDIS_CONFIG_FILE environment variable or place a configuration file named '{default_config_name}' in the 'configs' directory."
+        )
+
+    def __init__(self, config_file: str | None = None) -> None:
+        
+        if config_file is None:
+            config_file = self._default_config_file_path()
         
         # Check if file exists
         try:
             with open(config_file, 'r') as file:
                 self.config_data = file.read()
         except FileNotFoundError:
-            raise FileNotFoundError(f"Configuration file '{config_file}' not found.")
+            raise ConfigLoadError(f"Configuration file '{config_file}' not found.")
         except Exception as e:
             raise Exception(f"An error occurred while reading the configuration file: {e}")
         
         self.config = self.__parse_config()
     
-    def __parse_config(self) -> configparser.ConfigParser:
-        config = configparser.ConfigParser()
+    def __parse_config(self) -> ConfigParser:
+        config = ConfigParser()
         config.read_string(self.config_data)
 
         self.__parse_hardware_section(config)
@@ -29,46 +66,59 @@ class ARDISConfigParser:
 
         return config
     
-    def __parse_hardware_section(self, config: configparser.ConfigParser) -> None:
+    def __parse_hardware_section(self, config: ConfigParser) -> None:
         section = "Hardware"
         
         if not config.has_section(section):
-            raise ValueError(f"Missing '{section}' section in configuration file.")
+            raise ConfigOptionError(f"Missing '{section}' section in configuration file.")
         
         self.core_count = config.getint(section, "core_count")
         clock_domains_raw = config.get(section, "clock_domains")
         self.clock_domains: list[set[int]] = ast.literal_eval(clock_domains_raw)
 
-    def __parse_software_section(self, config: configparser.ConfigParser) -> None:
+    def __parse_software_section(self, config: ConfigParser) -> None:
         section = "Software"
         
         if not config.has_section(section):
             raise ValueError(f"Missing '{section}' section in configuration file.")
         self.temp_data_dir = config.get(section, "temp_data_dir")
 
-    def __parse_parsec_section(self, config: configparser.ConfigParser) -> None:
+    def __parse_parsec_section(self, config: ConfigParser) -> None:
         section = "Parsec_Benchmark"
         if not config.has_section(section):
             raise ValueError(f"Missing '{section}' section in configuration file.")
         
         self.parsec_base_dir = config.get(section, "benchmark_base_dir")
+        # Check if base_dir exists
+        if not os.path.isdir(self.parsec_base_dir):
+            raise ConfigOptionError(f"Parsec base directory '{self.parsec_base_dir}' does not exist.")
+
         self.parsec_available_packages_raw = config.get(section, "available_packages")
         self.parsec_available_packages: list[str] = self.parsec_available_packages_raw.strip().split()
 
-    def __parse_spec2006_section(self, config: configparser.ConfigParser) -> None:
+    def __parse_spec2006_section(self, config: ConfigParser) -> None:
         section = "Spec2006Benchmark"
         if not config.has_section(section):
-            raise ValueError(f"Missing '{section}' section in configuration file.")
+            raise ConfigOptionError(f"Missing '{section}' section in configuration file.")
         
         self.spec2006_base_dir = config.get(section, "benchmark_base_dir")
+        # Check if base_dir exists
+        if not os.path.isdir(self.spec2006_base_dir):
+            raise ConfigOptionError(f"SPEC2006 base directory '{self.spec2006_base_dir}' does not exist.")
+        
         self.spec2006_config_name = config.get(section, "config_file")
+        # Check if config file exists
+        config_path = os.path.join(self.spec2006_base_dir, "config", self.spec2006_config_name)
+        if not os.path.isfile(config_path):
+            raise ConfigOptionError(f"SPEC2006 configuration file '{config_path}' does not exist.")
+        
         self.spec2006_available_packages_raw = config.get(section, "available_packages")
         self.spec2006_available_packages: list[str] = self.spec2006_available_packages_raw.strip().split()
 
-    def __parse_experiment_section(self, config: configparser.ConfigParser) -> None:
+    def __parse_experiment_section(self, config: ConfigParser) -> None:
         section = "Experiment_Defaults"
         if not config.has_section(section):
-            raise ValueError(f"Missing '{section}' section in configuration file.")
+            raise ConfigOptionError(f"Missing '{section}' section in configuration file.")
         
         self.action_interval_ms = config.getint(section, "action_interval_ms")
         self.sampling_interval_ms = config.getint(section, "sampling_interval_ms")
