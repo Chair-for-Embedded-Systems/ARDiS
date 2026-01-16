b341aa11127cd7f7485dcb48d9e4a4a41c9ceed5
Andreas Noebel <uhqql@student.kit.edu>
2026-01-16 14:13:28 +0100

    feat: add multi choice prompt

--- ardis/utils/autoconfig/flows/prompts.py
+++ ardis/utils/autoconfig/flows/prompts.py
@@ -45,6 +45,63 @@ class SimplePrompts():
                     return page_items[selection_idx]
     
     @staticmethod
+    def multi_choice_prompt(
+        header_prompt: str,
+        choices: list[str],
+        max_items_per_page: int,
+        max_columns: int
+    ):
+        current_page = 0
+        total_pages = len(choices) // max_items_per_page + (1 if len(choices) == 0 else 0)
+        
+        selected_indices = set()
+        while True:
+            print("\033c", end="")
+            print(header_prompt)
+            # Items on this page
+            page_items = choices[current_page * max_items_per_page:min((current_page + 1) * max_items_per_page, len(choices))]
+
+            print(f"\n----------- Page {current_page + 1} of {total_pages} -----------\n")
+
+            # Print items in a grid if max_columns > 1 the index should be per column
+            items = []
+            for idx, item in enumerate(page_items):
+                global_idx = current_page * max_items_per_page + idx
+                mark = "[X]" if global_idx in selected_indices else "[ ]"
+                item = f"{global_idx + 1:>3}) {mark} {item}"
+                items.append(item)
+
+            SimplePrompts.print_item_grid(items, max_columns, column_offset=3, enable_index=False)
+
+            # Print page navigation info
+            print("\nEnter indices separated by spaces to toggle selection.")
+            print("Enter 'n' for next page, 'p' for previous page.")
+            print("Enter 'done' when finished.")
+            input_str = input(f"\n>>> ").strip()
+
+            # Handle navigation
+            if input_str.lower() == 'n':
+                current_page = (current_page + 1) % total_pages
+                continue
+            elif input_str.lower() == 'p':
+                current_page = (current_page - 1) % total_pages
+                continue
+            elif input_str.lower() == 'done':
+                break
+            else:
+                indices = input_str.split()
+                for index_str in indices:
+                    if index_str.isdigit():
+                        index = int(index_str) - 1
+                        if 0 <= index < len(choices):
+                            if index in selected_indices:
+                                selected_indices.remove(index)
+                            else:
+                                selected_indices.add(index)
+        
+        return [choices[i] for i in selected_indices]
+    
+    @staticmethod
     def print_item_grid(
         items: list[str],
         columns: int,
@@ -88,10 +145,20 @@ class SimplePrompts():
 if __name__ == "__main__":
     
     test_options = [f"Option {i}" for i in range(1, 101)]
-    item = SimplePrompts.single_choice_prompt(
-        header_prompt="Select an option:",
+    
+    items = SimplePrompts.multi_choice_prompt(
+        header_prompt="Select multiple options:",
         choices=test_options,
         max_items_per_page=16,
         max_columns=3,
     )
-    print(f"Selected option: {item}")
+    print(f"Selected items: {items}")
+    
+    
+    #item = SimplePrompts.single_choice_prompt(
+    #    header_prompt="Select an option:",
+    #    choices=test_options,
+    #    max_items_per_page=16,
+    #    max_columns=3,
+    #)
+    #print(f"Selected option: {item}")
