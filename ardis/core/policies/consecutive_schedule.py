516968ee9c8b6e039e67584f5e9fd0b21380e1c6
Andreas Noebel <uhqql@student.kit.edu>
2026-07-15 09:10:39 +0200

    feat: update consecutive scheduler

--- ardis/core/policies/consecutive_schedule.py
+++ ardis/core/policies/consecutive_schedule.py
@@ -1,16 +1,25 @@
-from ardis.core.scheduler import Scheduler
+from ardis.core.scheduler import Scheduler, Application
 
 class ConsecutiveScheduler(Scheduler):
-    def __init__(self, delay):
+    def __init__(self, delay_sec: float):
         super().__init__()
-        self.delay = delay
+        self._delay = delay_sec
+        self._schedule : dict[Application, float] = {}
+    
+    def register_workload(self, workload: list[Application]) -> None:
+        relative_start_time_sec = 0
+        for app in workload:
+            self._schedule[app] = relative_start_time_sec
+            relative_start_time_sec += self._delay
 
-
-    # Create a schedule for the applications
-    def createSchedule(self, applications):
-        # default schedule: all applications consecutively one after the other
-        t = 0
-        for app in applications:
-            self.schedule[app] = t*self.delay
-            t += 1
-        return self.schedule
+    def is_time_to_launch(self, application: Application, system_state) -> bool:
+        """
+        Check if the application is scheduled to start based on the current system time.
+        """
+        if application not in self._schedule:
+            raise ValueError(f"Application {application} not found in schedule.")
+        
+        scheduled_time = self._schedule[application]
+        current_time = system_state.elapsed_time_sec
+        
+        return current_time >= scheduled_time
